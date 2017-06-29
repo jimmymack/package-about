daa0e59cba3afe09d9a46a8283b3538f9149186a
Jacobi Petrucciani <jacobi@mimirhq.com>
2017-06-29 15:53:55 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -91,9 +91,30 @@ commands.register({
 //    }
 //});
 
-
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;domain=" + TLD;
+}
+function getCookie(cname) {
+    var name = cname + '=';
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return '';
+}
 // Open changes if version changes
 codebox.app.once("ready", function() {
-    // about();
+    if (!getCookie('mimirIDE-welcome')){
+        about();
+        setCookie('mimirIDE-welcome', 1, 100000);
+    }
 });
-
